ac20751bf93fcd58af2e8f9eb2690cb779bc511c
Ayush-byte-1203 <ayushpancholi3@gmail.com>
2026-07-21 19:06:53 +0530

    chnage

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,17 @@ export default defineConfig({
     watch: {
       ignored: ['**/backend/**']
     }
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 });
